6783429fdd3d21d4942ef658cf1b15cddf762b72
Philip Mumo <philipp1mogul@gmail.com>
2025-09-24 15:29:30 +0300

    Adding .Env Production to Environment

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,37 @@
-<?php
-
+<?php 
 namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        // Where we load in our views 
+        // return view('profile'); Will load profile view
+        // return view('about'); Will load about view
         return view('welcome_message');
+
     }
+
+    public function about()
+    {   
+        return view('about');
+    }
+
+    public function contact(): string
+    {
+        return view('contact');
+    }
+
+    public function profile(): string
+    {
+        return view('profile');
+    }
+
+
+    public function settings(): string
+    {
+        return view('settings');
+    }
+
+
 }
